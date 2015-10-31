e76e63de59e48b70265503a3bbdf6d9bbb7983c7
Андрей Букша <andreybuksha@icloud.com>
2015-10-31 20:21:54 +0300

    Created About View

---
+++ Favourite Place/AboutVC.swift
@@ -0,0 +1,28 @@
+//
+//  AboutVC.swift
+//  Favourite Place
+//
+//  Created by Андрей Букша on 31.10.15.
+//  Copyright © 2015 Андрей Букша. All rights reserved.
+//
+
+import UIKit
+
+class AboutVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+    
+    
+
+}
